02c2808101388f7f60ad3d7dfc495c2df3302a0c
TyGVne <100862858+TyGVne@users.noreply.github.com>
2022-03-03 23:43:32 +0700

    Update ip.js

--- cmds/ip.js
+++ cmds/ip.js
@@ -1,5 +1,5 @@
 exports.run = async (message, args)=> {
-    message.reply("```fix\nNhấn F8 nhập dòng dưới đây vào ấn Enter\r\nconnect cfx.re/join/3446er```");
+    message.reply("```fix\nNhấn F8 nhập dòng dưới đây vào ấn Enter\r\nconnect ndt90family.tk```");
 }
     
     
