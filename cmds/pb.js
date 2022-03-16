b727a32c5db49ccc702cd5f307aa3baa6b276e20
TyGVne <100862858+TyGVne@users.noreply.github.com>
2022-03-16 20:24:27 +0700

    ư

---
+++ cmds/pb.js
@@ -0,0 +1,65 @@
+exports.run = async (message, args)=> {
+    function cach(maxcach , length)
+    {
+      khoangcach = "                                                                                                                                                                                    "
+      return khoangcach.substring(0, maxcach - length)
+    }
+    
+    function hasnametag(nametagdata, namestring)
+    {
+      namestring = namestring.trim().toLowerCase();
+    
+      for (let i = 0; i < nametagdata.length; i++)
+        if (namestring.indexOf(nametagdata[i]) === 0) return true;
+      return false;
+    }
+    
+    try
+    {
+      data = await fetch(`http://${config.ip}/players.json`).then(d => d.json())
+    
+    } 
+    catch (error)
+    {
+      console.log(error)
+      return message.reply('`Server đang Offline`')
+    }
+  
+  
+    
+    for (let i = 0; i < config.pb.length; i++) {
+      let gangdata = config.pb[i]
+      let onlinelist = ''
+      let stt = 1
+   
+      
+      for (let j=0; j < data.length ; j++ ) { 
+        ten =data[j].name
+  
+  
+        if(hasnametag(gangdata.nametag , ten))
+        { 
+          onlinelist += '\n#' + stt + cach(4, stt.toString().length) + '[ID:' + data[j].id + ']' + cach(6 , data[j].id.toString().length ) + ': ' + data[j].name
+          stt++
+        }
+      
+      }
+    
+      if (onlinelist) {
+     
+        let embed = new Discord.MessageEmbed()
+        .setTitle(config.servername)
+        .setThumbnail(config.linkavt)
+        .setColor('RANDOM')
+        .addFields(
+          { name: `${gangdata.label} Online:`   , value: "```fix\n" + onlinelist + "```", inline: false },
+       
+        )
+        .setTimestamp();
+        message.reply(embed);
+    
+      }
+    
+    }
+  
+  }
